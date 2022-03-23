3127b5f20e1c6322cf3a0f0df61abf08e3e75e7c
Andrey <andrey9kovlev@mail.ru>
2022-03-23 13:04:50 +0300

    second commit

--- script.js
+++ script.js
@@ -11,11 +11,43 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = prompt(" На сколько оцеените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = prompt(" На сколько оцеените его?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// if (personalMovieDB.count < 10) {
+//     alert("Просмотрено мало фильмов");
+// } else if (personalMovieDB.count >= 10 && numberOfFilms <= 30) {
+//     alert("Вы классический зритель");
+// } else if (personalMovieDB.count > 30) {
+//     alert("Вы киноман");
+// } else {
+//     alert("Произошла ошибка");
+// };
+
+let message = (personalMovieDB.count) < 10 ? "Просмотрено мало фильмов":
+(personalMovieDB.count >= 10 && numberOfFilms <= 30) ? "Вы классический зритель":
+(personalMovieDB.count > 30) ? "Вы киноман":
+"Произошла ошибка";
+alert(message);
+
+// const a = prompt("Один из последних просмотренных фильмов?", ""),
+//     b = prompt(" На сколько оцеените его?", ""),
+//     c = prompt("Один из последних просмотренных фильмов?", ""),
+//     d = prompt(" На сколько оцеените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+// console.log(personalMovieDB);
+
+for (let i = 0; i < 2; i++) {
+    const a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt(" На сколько оцеените его?", "");
+
+
+    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    } else {
+        console.log("error");
+        i--;
+    }
+}
 console.log(personalMovieDB);
